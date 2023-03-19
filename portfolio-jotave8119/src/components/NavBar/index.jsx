2caf6d1e1797d20984d469f8a716ed4cdf1d03ca
jotave8119 <teixeirajoaovitor0@gmail.com>
2023-03-18 23:03:24 -0300

    add nav bar and responsive menu

--- portfolio-jotave8119/src/components/NavBar/index.jsx
+++ portfolio-jotave8119/src/components/NavBar/index.jsx
@@ -1,9 +1,53 @@
 import "./navbar.css";
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import profile from "../../assets/profile.png";
 
 const NavBar = () => {
-    return(
-        <nav className="topBar"></nav>
-    );
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsiveNav");
+  };
+
+  return (
+    <header>
+      <div className="photoBox">
+        <img className="photo" title="João" src={profile} alt="João" />
+        <h2 className="name">
+          João
+          <div class="wrapper">
+            <div class="circle"></div>
+            <div class="circle"></div>
+            <div class="circle"></div>
+            <div class="shadow"></div>
+            <div class="shadow"></div>
+            <div class="shadow"></div>
+        </div>
+        </h2>
+      </div>
+      <nav ref={navRef}>
+        <a href="" title="Sobre">
+          Sobre
+        </a>
+        <a href="" title="Projetos">
+          Projetos
+        </a>
+        <a href="" title="Tecnologias">
+          Tecnologias
+        </a>
+        <a href="" title="Contato">
+          Contato
+        </a>
+        <button className="openBtn closeBtn" onClick={showNavbar}>
+          <FaTimes size={20} />
+        </button>
+      </nav>
+
+      <button className="openBtn" onClick={showNavbar}>
+        <FaBars size={20} />
+      </button>
+    </header>
+  );
 };
 
 export default NavBar;
